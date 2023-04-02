88eb4b909a2989469e32d392a189c5892e9c3b58
wky <wangkaiyu1103@gmail.com>
2023-04-03 00:18:52 +0800

    feat: change banner pic

--- src/views/chat/layout/Permission.vue
+++ src/views/chat/layout/Permission.vue
@@ -70,7 +70,7 @@ function handlePress(event: KeyboardEvent) {
             {{ $t('common.unauthorizedTips') }}
           </p>
           <Icon403 class="w-[200px] m-auto" />
-          <p>网站宗旨为“构建无需翻墙稳定且快速的GPT对话服务”，近期随着使用人数暴增，网站运营需要大量成本，除了 `GPT3.5-Turbo` 接口费用，对网站安全检查与维护也需要一定成本，为了网站稳定运行，请点击红色按钮购买请求次数，后续OPENAI开放GPT4.0模型接口后也会第一时间接入；合作可+V: 17390763166</p>
+          <p>本站提供稳定且高速的GPT对话服务，现已支持GPT-4，但运营成本不断上升，GPT4接口费用为GPT3.5-Turbo费用的30倍。为让更多用户了解和体验，我们推出优惠活动，分享指定文案可获2千字个人授权码和共用授权码。您可关注公众号，了解具体步骤，也可直接购买服务。访问域名发布页获取更多服务信息。让GPT-3.5, GPT-4帮您更好地处理工作。合作+V: 17390763166</p>
         </header>
         <NInput v-model:value="token" type="password" placeholder="" @keypress="handlePress" />
         <NButton
